382b77e69eb227eaf9b264ab5a05f4236baa9a53
Trishia-K <kobu2rsha@gmail.com>
2024-05-23 17:07:44 +0300

    Add files via upload

--- Assignment 1.c
+++ Assignment 1.c
@@ -5,21 +5,21 @@ int main() {
     int num1;
     int num2;
     int num3;
-    printf("Enter number:");
+    printf("Enter a muliple of 3:");
     scanf("%d",&num1);
     if(num1%3==0){
     printf("Fizz\n",num1);}
     else{ 
         printf("Not a multiple of 3\n",num1);}
   //Checking if the number is a multiple of five  
-    printf("Enter number:");
+    printf("Enter a multiple of 5:");
     scanf("%d",&num2);
      if(num2%5==0){
         printf("Buzz\n",num2);}
      else{
          printf("Not a multiple of 5\n",num2);}
  //Checking if the number is a multile of seven
-    printf("Enter number:");
+    printf("Enter a multiple of 7:");
     scanf("%d",&num3);
     if(num3%7==0){
         printf("FizzBuzz\n",num3);} 
